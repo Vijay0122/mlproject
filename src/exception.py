c57cbbef4877794588053bc84e0f79e42d3156a7
Vijay0122 <vvrkumar0122@gmail.com>
2024-12-15 21:44:41 +0530

    Logging an exception

---
+++ src/exception.py
@@ -0,0 +1,35 @@
+import sys
+import logging
+
+# this will give us how a message should look like w.r.t custom exception 
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()   # this will give me execution info
+    
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occurred in python script name[{0}] line number[{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+    
+
+    
+
+class CustomException(Exception):
+    def _init_(self,error_message,error_detail:sys):
+        super._init(error_message)
+        self.error_message = error_message_detail(error_message, error_detail = error_detail)
+
+    def _self_(self):
+        return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        a=1/0
+    
+    except Exception as e:
+        logging.info("Divide by Zero Error")
+        raise CustomException(e)
+
+        
+        
+    
---
+++ src/logger.py
@@ -0,0 +1,21 @@
+# To log all the information and the file so that we can keep the track of all the errors
+
+import logging
+import os 
+from datetime import datetime
+
+LOG_FILE_ = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE_)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE_)
+
+
+logging.basicConfig(
+    filename = LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO,
+)
+
+if __name__ == "__main__":
+     logging.info("Logging Started")
